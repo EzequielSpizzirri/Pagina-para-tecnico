efe974455c757b597f0c58ddd414b5e6666c45f6
Ezequiel Spizzirri <ezequielspizzirri2000@gmail.com>
2023-08-31 17:27:39 -0300

    update

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     contactForm.addEventListener("submit", function (event) {
         event.preventDefault();
+
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
+        const mensaje = document.getElementById("mensaje").value;
+
+        // Llamada a la función para enviar los datos a Google Sheets
+        enviarDatosAGoogleSheets(nombre, email, mensaje);
+
         mensajeConfirmacion.classList.remove("hidden");
         contactForm.reset();
 
@@ -19,3 +27,30 @@ document.addEventListener("DOMContentLoaded", function () {
         body.classList.toggle('dark-mode');
     });
 });
+
+// Función para enviar los datos a Google Sheets
+function enviarDatosAGoogleSheets(nombre, email, mensaje) {
+    const apiKey = "AIzaSyDJOnAs-1lQbjP_t6ouFWhO89eNf8PYZFc"; // Reemplaza con tu clave de API
+    const spreadsheetId = "16czgSV0TiJ9k13cfTUx8A8Hjio72APs7ZOGMQtu05Is"; // Reemplaza con el ID de tu hoja de cálculo
+
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Formulario?key=${apiKey}`;
+    
+    const data = {
+        values: [[nombre, email, mensaje]]
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Datos enviados:', data);
+    })
+    .catch(error => {
+        console.error('Error al enviar los datos:', error);
+    });
+}
